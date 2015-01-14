6f4336148228e82b256d412977e2c1c2360ac96c
Joel Magee <jmagee@scottlogic.co.uk>
2015-01-14 16:42:23 +0000

    Fixed broken link and updated copyright dates

--- app/src/com/example/jmagee/chartlist/ChartListActivity.java
+++ app/src/com/example/jmagee/chartlist/ChartListActivity.java
@@ -13,7 +13,7 @@ import java.util.List;
 /**
  * Created by Joel Magee on 06/11/2014.
  *
- * Copyright (C) 2014 Scott Logic
+ * Copyright (C) 2015 Scott Logic
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
